f22a9082c092aa1a6cc2d3e2e4f18397d9f88dce
Peter A. <ink.splatters@pm.me>
2024-06-01 19:38:34 +0200

    Package.swift

---
+++ Package.swift
@@ -0,0 +1,11 @@
+// swift-tools-version: 5.10
+
+import PackageDescription
+
+let package = Package(
+    name: "Keymaster",
+    targets: [
+        .executableTarget(
+            name: "keymaster"),
+    ]
+)
